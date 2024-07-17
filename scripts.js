0de5bb395e0042cf61f140c7b679c843ac1cfdf5
ARPIT PRAJAPATI <getunknownerror@gmail.com>
2024-07-17 18:53:08 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
